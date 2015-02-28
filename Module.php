5cacf47524caa6418a3f7d60e7bb4389634d91f9
maxgu <maxgu@t4web.com.ua>
2015-02-28 18:38:05 +0200

    add ConfigProviderInterface and provide config for Abstract factories

--- Module.php
+++ Module.php
@@ -2,9 +2,14 @@
 namespace Base;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
-class Module implements AutoloaderProviderInterface
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
     public function getAutoloaderConfig()
     {

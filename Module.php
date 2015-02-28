0d3b1373e5be1c775d2754c378091d8764f1283f
maxgu <maxgu@t4web.com.ua>
2015-02-28 09:28:00 +0200

    + add composer.json + add Module.php

---
+++ Module.php
@@ -0,0 +1,20 @@
+<?php
+namespace Base;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements AutoloaderProviderInterface
+{
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+}

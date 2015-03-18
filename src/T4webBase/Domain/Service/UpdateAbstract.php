2f96332412d0aeadaa374b10012c8bda19d48788
maxgu <maxgu@t4web.com.ua>
2015-03-18 22:51:51 +0200

    update Service\Update - add getErrors()

--- src/T4webBase/Domain/Service/UpdateAbstract.php
+++ src/T4webBase/Domain/Service/UpdateAbstract.php
@@ -5,44 +5,54 @@ namespace T4webBase\Domain\Service;
 use T4webBase\InputFilter\InputFilterInterface;
 use T4webBase\Domain\Repository\DbRepository;
 use T4webBase\Domain\Criteria\Factory as CriteriaFactory;
+use T4webBase\InputFilter\InvalidInputError;
 use Zend\EventManager\EventManager;
 use T4webBase\Domain\EntityInterface;
 use Zend\Filter\Word\CamelCaseToUnderscore;
 
 abstract class UpdateAbstract implements UpdateInterface {
-    
+
     protected $inputFilter;
+
     /**
-     *
      * @var \T4webBase\Domain\Repository\DbRepository
      */
     protected $repository;
+
     /**
-     *
      * @var \T4webBase\Domain\Criteria\Factory
      */
     protected $criteriaFactory;
     protected $eventManager;
     protected $filterCamelCaseToUnderscore;
 
+    /**
+     * @var InvalidInputError
+     */
+    protected $errors;
+
     public function __construct(
-            InputFilterInterface $inputFilter,
-            DbRepository $repository,
-            CriteriaFactory $criteriaFactory,
-            EventManager $eventManager = null) {
-        
+        InputFilterInterface $inputFilter,
+        DbRepository $repository,
+        CriteriaFactory $criteriaFactory,
+        EventManager $eventManager = null) {
+
         $this->inputFilter = $inputFilter;
         $this->repository = $repository;
         $this->criteriaFactory = $criteriaFactory;
         $this->eventManager = $eventManager;
         $this->filterCamelCaseToUnderscore = new CamelCaseToUnderscore();
     }
-    
+
     public function isValid(array $data) {
         $this->inputFilter->setData($data);
-        return $this->inputFilter->isValid();
+        if (!$this->inputFilter->isValid()) {
+            $this->errors = new InvalidInputError($this->inputFilter->getMessages());
+            return false;
+        }
+        return true;
     }
-    
+
     abstract public function update($id, array $data);
 
     protected function trigger($event, EntityInterface $entity) {
@@ -59,49 +69,49 @@ abstract class UpdateAbstract implements UpdateInterface {
         if (!$entity) {
             return false;
         }
-        
+
         $entity->setActivated();
-        
+
         $this->repository->add($entity);
         $this->trigger('activate:post', $entity);
-        
+
         return true;
     }
-    
+
     public function inactivate($id) {
         /*@var $entity \T4webBase\Domain\Entity */
         $entity = $this->repository->find($this->criteriaFactory->getNativeCriteria('Id', $id));
         if (!$entity) {
             return false;
         }
-        
+
         $entity->setInactivated();
-        
+
         $this->repository->add($entity);
         $this->trigger('inactivate:post', $entity);
 
         return true;
     }
-    
+
     public function delete($id) {
         /*@var $entity \T4webBase\Domain\Entity */
         $entity = $this->repository->find($this->criteriaFactory->getNativeCriteria('Id', $id));
         if (!$entity) {
             return false;
         }
-        
+
         $entity->setDeleted();
-        
+
         $this->repository->add($entity);
         $this->trigger('delete:post', $entity);
 
         return true;
     }
-    
-    public function getMessages() {
-        return $this->inputFilter->getMessages();
+
+    public function getErrors() {
+        return $this->errors;
     }
-    
+
     public function getValues() {
         return $this->inputFilter->getValues();
     }

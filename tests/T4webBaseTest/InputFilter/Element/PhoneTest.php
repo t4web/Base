9d89cf1b87341d113e7cdd0e4f5895190b8b6be1
ros <r_toderash@t4web.com.ua>
2015-04-29 16:05:31 +0300

    fix test

--- tests/T4webBaseTest/InputFilter/Element/PhoneTest.php
+++ tests/T4webBaseTest/InputFilter/Element/PhoneTest.php
@@ -28,5 +28,30 @@ class PhoneTest extends \PHPUnit_Framework_TestCase {
             array('4564654', false),
         );
     }
+
+    /**
+     * @dataProvider providerWithInitData
+     */
+    public function testIsValid_InitPhoneWithData_Return($pattern, $errorPattern, $value, $result) {
+        $phone = new Phone('phone', $pattern, $errorPattern);
+        $phone->setValue($value);
+        $context['phone'] = $value;
+        $errorMessages = array(true => '', false => 'Неверный формат номера: '. $errorPattern);
+
+        $this->assertEquals($result, $phone->isValid($context));
+        $this->assertEquals($errorMessages[$result], $phone->getErrorMessage());
+    }
+
+    public function providerWithInitData() {
+        return array(
+            array('/^\+38 \(\d{3}\) \d{3}-\d{2}-\d{2}$/', '+38 (###) ###-##-##', '+38 (123) 123-12-12', true),
+            array('/^\+38 \(\d{3}\) \d{3}-\d{2}-\d{2}$/', '+38 (###) ###-##-##', '(123) 123-12-12', false),
+            array('/^\+38 \(\d{3}\) \d{3}-\d{2}-\d{2}$/', '+38 (###) ###-##-##', '', false),
+
+            array('/^\(\d{3}\) \d{3}-\d{2}-\d{2}$/', '(###) ###-##-##', '+38 (123) 123-12-12', false),
+            array('/^\(\d{3}\) \d{3}-\d{2}-\d{2}$/', '(###) ###-##-##', '(123) 123-12-12', true),
+            array('/^\(\d{3}\) \d{3}-\d{2}-\d{2}$/', '(###) ###-##-##', '', false),
+        );
+    }
     
 }
